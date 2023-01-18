b1c59355200b69cfa73d5fcde1fbe53363a67b3d
smhrd <smhrd@125.136.57.25>
2023-01-18 11:45:02 +0900

    UserDTO

---
+++ ClassLight/src/main/java/com/smhrd/model/UserDTO.java
@@ -0,0 +1,106 @@
+package com.smhrd.model;
+
+public class UserDTO {
+
+	    // 회원 아이디 
+	    private String userId;
+
+	    // 회원 패스워드 
+	    private String userPw;
+
+	    // 회원 이름 
+	    private String userName;
+
+	    // 회원 가입일자 
+	    private String userJoindate;
+
+	    // 회원 생년월일 
+	    private String userBirthdate;
+
+	    // 회원 포인트 
+	    private Double userPoint;
+
+	    // 회원 유형 
+	    private String userType;
+
+	    // 시리얼 넘버 
+	    private String serialNo;
+
+	    public String getUserId() {
+	        return userId;
+	    }
+
+	    public void setUserId(String userId) {
+	        this.userId = userId;
+	    }
+
+	    public String getUserPw() {
+	        return userPw;
+	    }
+
+	    public void setUserPw(String userPw) {
+	        this.userPw = userPw;
+	    }
+
+	    public String getUserName() {
+	        return userName;
+	    }
+
+	    public void setUserName(String userName) {
+	        this.userName = userName;
+	    }
+
+	    public String getUserJoindate() {
+	        return userJoindate;
+	    }
+
+	    public void setUserJoindate(String userJoindate) {
+	        this.userJoindate = userJoindate;
+	    }
+
+	    public String getUserBirthdate() {
+	        return userBirthdate;
+	    }
+
+	    public void setUserBirthdate(String userBirthdate) {
+	        this.userBirthdate = userBirthdate;
+	    }
+
+	    public Double getUserPoint() {
+	        return userPoint;
+	    }
+
+	    public void setUserPoint(Double userPoint) {
+	        this.userPoint = userPoint;
+	    }
+
+	    public String getUserType() {
+	        return userType;
+	    }
+
+	    public void setUserType(String userType) {
+	        this.userType = userType;
+	    }
+
+	    public String getSerialNo() {
+	        return serialNo;
+	    }
+
+	    public void setSerialNo(String serialNo) {
+	        this.serialNo = serialNo;
+	    }
+
+	    // UserTb 모델 복사
+	    public void CopyData(UserDTO param)
+	    {
+	        this.userId = param.getUserId();
+	        this.userPw = param.getUserPw();
+	        this.userName = param.getUserName();
+	        this.userJoindate = param.getUserJoindate();
+	        this.userBirthdate = param.getUserBirthdate();
+	        this.userPoint = param.getUserPoint();
+	        this.userType = param.getUserType();
+	        this.serialNo = param.getSerialNo();
+	    }
+	
+}
