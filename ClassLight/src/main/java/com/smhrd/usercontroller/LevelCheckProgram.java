26f3222255b916e2bf11b12233772b687e9e8d40
smhrd <smhrd@121.147.185.43>
2023-01-20 11:41:51 +0900

    controller package 수정 - 채영

--- ClassLight/src/main/java/com/smhrd/usercontroller/LevelCheckProgram.java
+++ ClassLight/src/main/java/com/smhrd/usercontroller/LevelCheckProgram.java
@@ -1,34 +1,40 @@
-package com.smhrd.controller;
+package com.smhrd.usercontroller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.model.LevelCheckDAO;
+import com.smhrd.model.LevelCheckDTO;
 import com.smhrd.model.UserDAO;
 import com.smhrd.model.UserDTO;
 
-public class ResearchId implements Command {
+public class LevelCheckProgram implements Command {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String result = null;
 		
-		String name = request.getParameter("name");
+		LevelCheckDAO dao = new LevelCheckDAO();
 		
-	    UserDTO dto = new UserDTO();
-	    
-	    dto.setUserName(name);
-	    
-	    UserDAO dao = new UserDAO();
-	    
-	    UserDTO row = dao.researchId(dto);
+		List<LevelCheckDTO> list = dao.selectAll();
 		
-		return result;
+		if(list != null) {
+			request.setAttribute("std_id", list);
+		}
+		
+		
+		
+		return "select.jsp";
 	}
+
+	
+	
 
 }
