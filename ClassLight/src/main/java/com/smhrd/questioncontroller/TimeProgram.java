3bc8fec5510b9bdc3b030fff642511ad35abe78c
220in <121835105+220in@users.noreply.github.com>
2023-01-30 19:42:04 +0900

    dd

--- ClassLight/src/main/java/com/smhrd/questioncontroller/TimeProgram.java
+++ ClassLight/src/main/java/com/smhrd/questioncontroller/TimeProgram.java
@@ -1,6 +1,7 @@
 package com.smhrd.questioncontroller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,23 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.smhrd.model.QuestionDAO;
 import com.smhrd.model.QuestionDTO;
 import com.smhrd.usercontroller.Command;
 
-public class TimeProgram extends HttpServlet implements Command {
+public class TimeProgram implements Command {
 
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+        String result = null;
 		QuestionDAO dao = new QuestionDAO();
-
 		List<QuestionDTO> timeList = dao.time();
+        PrintWriter out = response.getWriter();
 
 		if (timeList != null) {
-			request.getSession().setAttribute("q_time", timeList);
+			Gson gson = new Gson();
+			out.print(gson.toJson(timeList));
+			System.out.println("들어옴 >>> " + timeList.get(3).getQTitle());
+			
 		} 
-		return "";
+		return null;
 	}
 
 }
